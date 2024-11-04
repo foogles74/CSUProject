8c9182ba34fa59796c34e4b87124aed9cd5c3606
foogles74 <foogles74@gmail.com>
2024-11-04 17:53:47 +0500

    first

---
+++ app/db/models/User.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String
+
+from app.db.BaseModel import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = 'users'
+
+    user_id = Column(autoincrement= True,type=String, nullable=False, unique=True)
+    login = Column(type=String, nullable=False, unique=True)
+    password = Column(type=String, nullable=False)
+    email = Column(type=String, nullable=False, unique=True)
