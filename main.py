6f97115e6bf11afa0d4e1dfc070606cab398334c
foogles74 <foogles74@gmail.com>
2024-11-10 20:16:16 +0500

    test

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from db.DBConnect import Base, DBConnect
+from sqlmodel import Session
+
+from db.dao.user import create_user, get_all_users
+from db.database import init_db, engine
+from db.models.user import User
 
 
-def init_db():
-    connector = DBConnect()
-    Base.metadata.create_all(bind=connector.engine)
-
-def main():
+if __name__ == "__main__":
+    test_user = User(login = "test",email="test@mail.ru", password="test")
+    print("Подключение к db")
     init_db()
+    print("db")
+
+    with Session(engine) as session:
+        create_user(test_user, session)
+
+        users = get_all_users(session)
+
+    for user in users:
+        # print(f'id: {user.id} - {user.email}')
+        print(user)
