33b4485bff138a7c7477dfcc0cb3f16834ae887d
foogles74 <foogles74@gmail.com>
2024-11-12 00:02:20 +0500

    test

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from sqlmodel import Session
 
+from models.qwen.qwem_model import generate_text
 from db.dao.user import create_user, get_all_users
 from db.database import init_db, engine
 from db.models.user import User
@@ -17,3 +18,4 @@ if __name__ == "__main__":
     for user in users:
         # print(f'id: {user.id} - {user.email}')
         print(user)
+    generate_text("Что такое ИИ?")
