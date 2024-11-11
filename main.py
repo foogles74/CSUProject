06a4e6ebc34f0daa8c9620e2a8d4ddc30511a12d
foogles74 <foogles74@gmail.com>
2024-11-11 23:20:17 +0500

    test

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ from db.models.user import User
 
 
 if __name__ == "__main__":
-    test_user = User(login = "test",email="test@mail.ru", password="test")
+
     print("Подключение к db")
     init_db()
     print("db")
 
     with Session(engine) as session:
-        create_user(test_user, session)
-
         users = get_all_users(session)
 
     for user in users:
