a6af6a30489c35139acfd8b8077a7d493eec3e0e
foogles74 <foogles74@gmail.com>
2024-11-12 23:19:33 +0500

    test

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from sqlmodel import Session
 from torch.utils.hipify.hipify_python import value
 
+from models.qwen.qwem_model import generate_text
 from db.models.transaction import Transaction
 from db.models.user import User
 from db.dao.transactionDAO import get_all_transactions,create_transaction,get_history_transactions
@@ -9,34 +10,36 @@ from db.database import init_db, engine
 
 
 if __name__ == "__main__":
-    user_admin = User(login = "admin",email="admin@admin", password="123qwe",balance=999999)
-    user_test = User(login="test", email="test@admin", password="123qwe", balance=0)
-    print("Подключение к db")
-    try :
-        init_db()
-        print("База Подключена")
-    except Exception as e:
-        print("Ошибка Подключения к БД по причине: "+str(e))
-        exit(1)
+    generate_text("Что такое ИИ?")
 
-    new_transactions = Transaction(user_id=1,value = 1)
-    new_test_transactions1 = Transaction(user_id=2, value=10)
-    new_test_transactions2 = Transaction(user_id=2, value=10)
+    # user_admin = User(login = "admin",email="admin@admin", password="123qwe",balance=999999)
+    # user_test = User(login="test", email="test@admin", password="123qwe", balance=0)
+    #
+    # print("Подключение к db")
+    # try :
+    #     init_db()
+    #     print("База Подключена")
+    # except Exception as e:
+    #     print("Ошибка Подключения к БД по причине: "+str(e))
+    #     exit(1)
+    #
+    # new_transactions = Transaction(user_id=1,value = 1)
+    # new_test_transactions1 = Transaction(user_id=2, value=10)
+    # new_test_transactions2 = Transaction(user_id=2, value=10)
+    #
+    # with Session(engine) as session:
+    #     users = get_all_users(session)
+    #     create_user(user_admin,session)
+    #     create_user(user_test, session)
+    #     transaction = get_all_transactions(session)
+    #     create_transaction(new_transactions,session)
+    #
+    #     create_transaction(new_test_transactions1, session)
+    #     create_transaction(new_test_transactions2, session)
+    #
+    #     userTransactions = get_history_transactions(2,session)
+    #
+    #
+    # for user in userTransactions:
+    #     print(user)
 
-    with Session(engine) as session:
-        users = get_all_users(session)
-        create_user(user_admin,session)
-        create_user(user_test, session)
-        transaction = get_all_transactions(session)
-        create_transaction(new_transactions,session)
-
-        create_transaction(new_test_transactions1, session)
-        create_transaction(new_test_transactions2, session)
-
-        userTransactions = get_history_transactions(2,session)
-
-
-    for user in userTransactions:
-        # print(f'id: {user.id} - {user.email}')
-        print(user)
-    # generate_text("Что такое ИИ?")
